a7fb476312f1711dc8efc5c051f05ac4b1581c35
LEONARDO DE OLIVEIRA CAMPOS <200203@facens.br>
2021-04-29 11:25:37 -0300

    add exercise

---
+++ src/main/java/Operation.java
@@ -0,0 +1,22 @@
+package poo.ex001.math.operation;
+
+public class Operation {
+    
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+    
+}
+
